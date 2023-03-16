af7e16b4c1fd82ffe59fb129ca0661bba3d9009e
Cral-Cactus <john.j.smithg@gmail.com>
2023-03-16 18:34:18 +0200

    Upload

---
+++ MultiplyEvenByOdd/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MultiplyEvenByOdd
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMultipleOfEvensAndOdds(n));
+        }
+
+        static int GetMultipleOfEvensAndOdds(int n)
+        {
+            int sumEvens = GetSumOfEvenDigits(Math.Abs(n));
+            int sumOdds = GetSumOfOddDigits(Math.Abs(n));
+            
+            return sumEvens * sumOdds;
+        }
+
+        static int GetSumOfOddDigits(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 != 0)
+                {
+                    sum += lastDigit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+
+        static int GetSumOfEvenDigits(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 == 0)
+                {
+                    sum += lastDigit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+    }
+}
---
+++ PriceChangeAlert/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PriceChangeAlert
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double significanceThreshold = double.Parse(Console.ReadLine());
+            double previousPrice = double.Parse(Console.ReadLine());
+            
+            for (int i = 1; i < n; i++)
+            {
+                double currentPrice = double.Parse(Console.ReadLine());
+                double differencePercentage = GetDifferencePercentage(previousPrice, currentPrice);
+                
+                bool isSignificant = Math.Abs(differencePercentage) > significanceThreshold;
+                string message = GetMessage(currentPrice, previousPrice, differencePercentage, isSignificant);
+                
+                Console.WriteLine(message);
+                
+                previousPrice = currentPrice;
+            }
+        }
+
+        static double GetDifferencePercentage(double previousPrice, double currentPrice)
+        {
+            return (currentPrice - previousPrice) / previousPrice * 100;
+        }
+
+        static string GetMessage(double currentPrice, double previousPrice, double differencePercentage, bool isSignificant)
+        {
+            if (differencePercentage == 0)
+            {
+                return string.Format($"NO CHANGE: {currentPrice}");
+            }
+            else if (!isSignificant)
+            {
+                return string.Format($"MINOR CHANGE: {previousPrice} to {currentPrice} ({differencePercentage:F2}%)");
+            }
+            else if (differencePercentage > 0)
+            {
+                return string.Format($"PRICE UP: {previousPrice} to {currentPrice} ({differencePercentage:F2}%)");
+            }
+            else
+            {
+                return string.Format($"PRICE DOWN: {previousPrice} to {currentPrice} ({differencePercentage:F2}%)");
+            }
+        }
+    }
+}
